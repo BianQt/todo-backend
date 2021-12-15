4aabd3d858a631a34950520ff76052772e937124
BianQt <bayanqutshan@gmail.com>
2021-12-15 23:52:37 +0200

    Signin Route fix

--- src/auth/routes.js
+++ src/auth/routes.js
@@ -28,6 +28,13 @@ authRouter.post('/signin', basicAuth, (req, res, next) => {
   };
   res.status(200).json(user);
 });
+authRouter.post('/signinI', bearerAuth, (req, res, next) => {
+  const user = {
+    user: req.user,
+    token: req.user.token
+  };
+  res.status(200).json(user);
+});
 
 authRouter.get('/users', bearerAuth, permissions('delete'), async (req, res, next) => {
   const userRecords = await users.findAll({});
